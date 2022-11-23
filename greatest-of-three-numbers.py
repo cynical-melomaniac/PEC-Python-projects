d0bf880cd326cd3884668babb6f5933823e5f644
PD <dhimanparth16.gmail.com>
2022-11-23 10:17:55 +0530

    On branch master Changes to be committed: 	modified:   greatest-of-three-numbers.py

--- greatest-of-three-numbers.py
+++ greatest-of-three-numbers.py
@@ -1,10 +1,14 @@
-a = eval(input("Enter number 1: "))
-b = eval(input("Enter number 2: "))
-c = eval(input("Enter number 3: "))
+try:
+    a = eval(input("Enter number 1: "))
+    b = eval(input("Enter number 2: "))
+    c = eval(input("Enter number 3: "))
+    
+    if a > b and a > c:
+        print(a, "is the greatest")
+    if b > a and b > c:
+        print(b, "is the greatest")
+    if c > a and c > b:
+        print(c, "is the greatest")
 
-if a > b and a > c:
-    print(a, "is the greatest")
-if b > a and b > c:
-    print(b, "is the greatest")
-if c > a and c > b:
-    print(c, "is the greatest")
+except:
+    print ("Enter a valid number!")
