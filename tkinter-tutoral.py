75be18b609fa8f67af8cc6776acaa876f2c45db6
PD <dhimanparth16.gmail.com>
2022-11-27 10:51:25 +0530

    Changes to be committed: 	modified:   tkinter-tutoral.py

--- tkinter-tutoral.py
+++ tkinter-tutoral.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 
-class Application(tk.Frame):              
+class Application(tk.Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)   
-        self.grid()                       
+        tk.Frame.__init__(self, master)
+        self.grid()
         self.createWidgets()
 
     def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)            
-        self.quitButton.grid()            
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton.grid()
 
-app = Application()                       
-app.master.title('Sample application')    
-app.mainloop()                      
+app = Application()
+app.master.title('Sample application')
+app.mainloop()
