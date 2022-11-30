fdf65e357b297d99f641641e1a513888f337d012
PD <dhimanparth16.gmail.com>
2022-11-30 10:26:44 +0530

    Update add-different-types-of-objects.py

--- add-different-types-of-objects.py
+++ add-different-types-of-objects.py
@@ -1,7 +1,7 @@
-a = 25
-b = eval("25")
-c = int(25.0)
+a = 25              #int
+b = eval("25")      #string to int
+c = int(25.0)       #float to int
 
-sum = str(a+b+c)
+sum = str(a+b+c)    #int to str
 
-print(sum)
+print(sum)          #output sum
