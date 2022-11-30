c9bbf2287c0bb00492e4079c25f124b652ca06ac
PD <dhimanparth16.gmail.com>
2022-11-30 10:08:13 +0530

    Changes to be committed: 	new file:   income-tax-calculator.py

---
+++ income-tax-calculator.py
@@ -0,0 +1,26 @@
+#To calculate Income Tax
+
+try:
+    #Get gross income
+    income = eval(input("Enter Gross Income: "))
+    
+    standard_deduction = 10000      #Standard Deduction
+
+    #Get number of dependents
+    dependents = eval(input("Enter number of dependents: "))
+    dependent_deduction = 3000      #Deduction on each individual dependent
+    
+    #Calculate taxable income
+    taxable_income = income - standard_deduction - (dependent_deduction * dependents)
+
+    #Prevent taxable income to go into negative
+    if(taxable_income < 0):
+        taxable_income = 0
+
+    #Display taxable income
+    print("Taxable income is: ", taxable_income)
+
+    print("Therefore tax applicable is: ", taxable_income * 0.2)
+
+except:
+    print ("The data entered is invalid. Exiting Application...")
