a2fbe91bc4bca2073411afd5b8f2645bac956fbc
PD <dhimanparth16.gmail.com>
2022-11-30 18:52:55 +0530

    Changes to be committed: 	modified:   Assignment-1/add-different-types-of-objects.py

--- Assignment-1/add-different-types-of-objects.py
+++ Assignment-1/add-different-types-of-objects.py
@@ -1,6 +1,6 @@
 a = 25                      #int
-b = ("25")              #string to int
-c = (25.0)               #float to int
+b = eval("25")              #string to int
+c = int(25.0)               #float to int
 
 sum = str(a+b+c)            #int to str
 
