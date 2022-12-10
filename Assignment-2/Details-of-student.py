3ca90e235b126f1c9a00e1facced985cb1cc5a6d
PD <cynical.melomaniac@gmail.com>
2022-12-10 06:53:10 +0530

    Changes to be committed: 	new file:   Assignment-2/Bitwise-operators.py 	new file:   Assignment-2/Details-of-student.py 	new file:   Assignment-2/Triangle-checking.py 	renamed:    greatest-of-three-numbers.py -> Assignment-2/greatest-of-three-numbers.py 	new file:   Assignment-2/string-manipulation.py 	new file:   Assignment-2/substring-checking.py

---
+++ Assignment-2/Details-of-student.py
@@ -0,0 +1,8 @@
+name = str("Parth")
+sid = int("22106026")
+department_name = str("CSE DS")
+cgpa = float("10.0")
+
+print("Hey,", name, "here!")
+print("My SID is", sid)
+print("I am from",department_name, "department and my CGPA is", cgpa)
---
+++ Assignment-2/substring-checking.py
@@ -0,0 +1,9 @@
+input_string = input("Input a string:")
+
+input_name = input("Enter a name:")
+
+if input_name in input_string:
+    print("Yes")
+
+else:
+    print("No")
