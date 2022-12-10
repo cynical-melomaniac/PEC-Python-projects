9cf295d703719876264b0c7cb622ab5344ac3487
PD <cynical.melomaniac@gmail.com>
2022-12-10 06:59:44 +0530

    Update Triangle-checking.py

--- Assignment-2/Triangle-checking.py
+++ Assignment-2/Triangle-checking.py
@@ -1,2 +1,20 @@
 try:
-    side_a = 
+    side_a = eval(input("Enter the first side of the triangle:"))
+    side_b = eval(input("Enter the first side of the triangle:"))
+    side_c = eval(input("Enter the first side of the triangle:"))
+    
+    if (side_a + side_b) > side_c:
+        if(side_b + side_c) > side_a:
+            if(side_a + side_c) > side_b:
+                print("Yes")
+
+            else:
+                print("No")
+        else:
+            print("No")
+    else:
+        print("No")
+
+except:
+    print("Invalid value entered. Exiting application...")
+
