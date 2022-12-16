996f3332e2a861a6af7c620403b22d60f86eaa88
Cynical <cynical.melomaniac@gmail.com>
2022-12-16 15:17:35 +0530

    Changes to be committed: 	modified:   Assignment-2/Details-of-student.py

--- Assignment-2/Details-of-student.py
+++ Assignment-2/Details-of-student.py
@@ -1,8 +1,13 @@
-name = str("Parth")
-sid = int("22106026")
-department_name = str("CSE DS")
-cgpa = float("10.0")
+# name = str("Parth")
+# sid = int("22106026")
+# department_name = str("CSE DS")
+# cgpa = float("10.0")
+
+name = str(input("Enter your name: "))
+sid = int(input("Enter your sid:"))
+department_name = str(input("Enter your department name:"))
+cgpa = float(input("Enter your cgpa:"))
 
 print("Hey,", name, "here!")
 print("My SID is", sid)
-print("I am from",department_name, "department and my CGPA is", cgpa)
+print("I am from", department_name, "department and my CGPA is", cgpa)
