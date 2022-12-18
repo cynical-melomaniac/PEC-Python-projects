38d5ab96f7ea1507d1ed19ed9a6adf9faa60bdc0
Cynical <cynical.melomaniac@gmail.com>
2022-12-18 18:29:58 +0530

    Changes to be committed: 	modified:   Assignment-3/dictionary.py

--- Assignment-3/dictionary.py
+++ Assignment-3/dictionary.py
@@ -1,16 +1,28 @@
+#Dictionary containing raw student data
 student_dict = {}
 
+#Dictionary containing SID sorted student data
+sid_sorted_student_dict = {}
+
+#Dictionary containing Name sorted student data
+name_sorted_student_dict = {}
+
+#Integer used to manage the option input for non student data entry prompt
 option = int(10)
 
+#while loop for reoccuring prompt
 while (option != 0):
 
+    #Option input used for entering of student data
     data_option = str(input("Enter student data? (Y/N):"))
 
+    #Code block for data entry
     if (data_option == "Y"):
-        _sid = int(input("Enter SID: "))
-        _name = str(input("Enter student name: "))
-        student_dict[_sid] = _name
+        _sid = int(input("Enter SID: "))                #Get SID
+        _name = str(input("Enter student name: "))      #Get Name
+        student_dict[_sid] = _name                      #Add SID and Name into the dictionary as a Key-Value Pair
 
+    #Code block for operations on the student_dict
     if (data_option == "N"):
 
         print("1) Print student details in dictionary")
@@ -21,14 +33,23 @@ while (option != 0):
         option = int(input("Enter an option: "))
 
         if (option == 1):
+            #Print raw student data
             print(student_dict)
 
         elif (option == 2):
-            print("Dude")
+            #Sort student data by name
+
+            #Get items in the dict (i.e. student_dict.items()), create a new small (lambda) function for the sorting mechanism of the sorted() function
+            name_sorted_student_dict = dict(sorted(student_dict.items(), key = lambda x : x[1]))
+            print(name_sorted_student_dict)
 
         elif (option == 3):
-            print("atleast teach the fucking algo.")
+            #Sort student data by SID(Key) by iterating for each SID present in student_dict in ascending order
+            for i in sorted(student_dict):
+                sid_sorted_student_dict[i] = student_dict[i]
+            print(sid_sorted_student_dict)
 
         elif (option == 4):
+            #Search by SID
             _search_sid = int(input("Enter SID to search: "))
             print(student_dict[_search_sid])
