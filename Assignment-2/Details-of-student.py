f14e08c1e5419c66d5caf872369ac2e8b15ea6e6
Cynical <cynical.melomaniac@gmail.com>
2022-12-19 20:29:59 +0530

    Changes to be committed: 	modified:   Assignment-2/Details-of-student.py

--- Assignment-2/Details-of-student.py
+++ Assignment-2/Details-of-student.py
@@ -3,6 +3,6 @@ sid = int("22106026")
 department_name = str("CSE DS")
 cgpa = float("10.0")
 
-print("\nHey,", name, "here!")
+print("Hey,", name, "here!")
 print("My SID is", sid)
 print("I am from", department_name, "department and my CGPA is", cgpa)
