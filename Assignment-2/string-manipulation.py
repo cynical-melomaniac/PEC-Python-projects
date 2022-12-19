38b1222c471a55598cdc01bb18fed2ed8c1b31ad
Cynical <cynical.melomaniac@gmail.com>
2022-12-19 20:26:16 +0530

    Changes to be committed: 	modified:   Assignment-2/string-manipulation.py

--- Assignment-2/string-manipulation.py
+++ Assignment-2/string-manipulation.py
@@ -1,16 +1,23 @@
+#To manipulate the given string in given methods
 given_string = "Python is a case sensitive language"
 
+#Length of string
 print("Length of string:", len(given_string))
 
+#Reverse string
 print("String Reversed:", given_string [::-1])
 
+#Slicing of string
 sliced_string = given_string [slice(10,27)]
 print("Sliced String is:", sliced_string)
 
+#Replacement of a phrase in string
 replaced_string = given_string.replace("a case sensitive", "object oriented")
 print("String replaced 'a case sensitive' with 'object oriented' is:", replaced_string)
 
+#Index of substring
 print("Index of substring 'a':",given_string.index("a"))
 
+#Remove whitespaces
 removed_whitespaces_string = given_string.replace(" ","")
 print("String with removed whitespaces:",removed_whitespaces_string)
