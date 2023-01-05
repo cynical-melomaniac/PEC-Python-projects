6383f191ce5fc6842f325f146c1a4157555c0c8d
Cynical <cynical.melomaniac@gmail.com>
2023-01-05 10:03:23 +0530

    Changes to be committed: 	modified:   Assignment-3/next-date.py

--- Assignment-3/next-date.py
+++ Assignment-3/next-date.py
@@ -4,28 +4,46 @@ year = int(input("Enter year: "))
 
 oddMonths = [1, 3, 5, 7, 8, 10, 12]
 
-if date >= 1 and month >= 1 and year >= 1800:
-    if date <= 31 and month <= 12 and year <= 2025:
-        date += 1
 
-if date > 28 and month == 2:
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        date += 1
-
-if date > 29 and month == 2:
-    date = 1
-    month += 1
-
-elif date > 30 and oddMonths.index(month) is not int:
-    date = 1
-    month += 1
-
-elif date > 31:
-    date = 1
-    month += 1
-    if month == 12:
+if month in oddMonths:
+    date += 1
+    if date > 31:
         date = 1
-        month = 1
-        year += 1
+        month += 1
+
+if month not in oddMonths:
+    
+    date += 1
+    
+    if month != 2:
+        if date > 30:
+            date = 1
+            month += 1
+
+    if month == 2:
+        
+        leap_year = False
+        
+        if year % 4 == 0:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    leap_year = True
+                else:
+                    leap_year = False
+            else:
+                leap_year = True
+        else:
+            leap_year = False
+
+        if leap_year:
+            if date > 29:
+                date = 1
+                month += 1
+        
+        if not leap_year:
+            if date > 28:
+                date = 1
+                month += 1
+
 
 print("Next date is: ", date, "/", month, "/", year)
