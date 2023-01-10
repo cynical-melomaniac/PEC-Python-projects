0409712efe3ad65973091dfedd0e12a3bd57fc5e
Cynical <cynical.melomaniac@gmail.com>
2023-01-10 13:44:46 +0530

    print-alphabet-pattern.py commit

--- Assignment-5/print-alphabet-pattern.py
+++ Assignment-5/print-alphabet-pattern.py
@@ -1,8 +1,11 @@
 rows = int(input("Enter number of rows to be input:"))
 
-alphabets = str("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+count = 0
 
-for i in range(0, rows):
-    for j in range(i): 
-        print(alphabets[i + j - 1], end="")
+for i in range(0, rows):   
+    for j in range(0, i + 1):
+        if (count >= 26):
+            count = 0
+        print(chr(65 + count), end = '')
+        count += 1
     print("")
