66991bac9824b930bdba528a79bed5d9fb14078d
Cynical <cynical.melomaniac@gmail.com>
2023-01-11 21:36:14 +0530

    Changes to be committed: 	modified:   Assignment-5/integer-set.py

--- Assignment-5/integer-set.py
+++ Assignment-5/integer-set.py
@@ -6,33 +6,49 @@ while(i < 10):
     integer_set.append(integer_set_input)
     i += 1
 
-print("Positive numbers:")
-for j in integer_set:
-    if (j > 0):
-        print(j)
+# Positive number
+positive_number_list = list()
 
-print("Negative numbers")
-for j in integer_set:
-    if (j < 0):
-        print(j)
+for positive_number in integer_set:
+    if (positive_number > 0):
+        positive_number_list.append(positive_number)
 
-print("Odd numbers")
-for j in integer_set:
-    if (j % 2 != 0):
-        print(j)
+print("Positive numbers: ", positive_number_list)
 
-print("Even numbers")
-for j in integer_set:
-    if (j % 2 == 0):
-        print(j)
+# Negative number
+negative_number_list = list()
 
+for negative_number in integer_set:
+    if (negative_number < 0):
+        negative_number_list.append(negative_number)
+
+print("Negative numbers: ", negative_number_list)
+
+# Odd number
+odd_number_list = list()
+
+for odd_number in integer_set:
+    if (odd_number % 2 != 0):
+        odd_number_list.append(odd_number)
+
+print("Odd numbers: ", odd_number_list)
+
+#Even numbers
+even_number_list = list()
+
+for even_number in integer_set:
+    if (even_number % 2 == 0):
+        even_number_list.append(even_number)
+
+print("Even numbers", even_number_list)
+
+# Number of times each number occurs in the List
 number_dict = dict()
 
-print("Number of time each item occurs in list")
-for j in integer_set:
-    if j in number_dict:
-        number_dict[j] += 1
-    if j not in number_dict:
-        number_dict[j] = 1
+for number in integer_set:
+    if number in number_dict:
+        number_dict[number] += 1
+    if number not in number_dict:
+        number_dict[number] = 1
         
-print(number_dict)
+print("Number of time each item occurs in list", number_dict)
